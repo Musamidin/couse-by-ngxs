037d42b64e85c343ce30e508766b1d98537dcfdf
Musa <m.ahmedov@transcom.gov.kg>
2020-11-10 23:38:29 +0600

    *barcode scandit-engine-sdk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SimpleCalendarComponent, HeatMapModel, ColorMapModel } from './simple-calendar/simple-calendar.component';
 import { SimpleDate } from 'src/app/simple-calendar/simple-calendar.component';
+import { Barcode, BarcodePicker, ScanSettings, configure } from 'scandit-sdk-angular';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,15 +9,25 @@ import { SimpleDate } from 'src/app/simple-calendar/simple-calendar.component';
 })
 export class AppComponent {
 
-
+  val: string;
+  public settings = new ScanSettings({ enabledSymbologies: [
+      Barcode.Symbology.EAN8,
+      Barcode.Symbology.EAN13,
+      Barcode.Symbology.UPCA,
+      Barcode.Symbology.UPCE,
+      Barcode.Symbology.CODE128,
+      Barcode.Symbology.CODE39,
+      Barcode.Symbology.CODE93,
+      Barcode.Symbology.INTERLEAVED_2_OF_5,
+    ] });
 
   public selDate: SimpleDate = { date: 1, month: 1, year: 1 };
   heatmap: HeatMapModel;
-  highlightToday: boolean
+  highlightToday: boolean;
   colorMap: ColorMapModel;
   zeropad: (num: any, padlen: any, padchar?: string) => string;
   today: Date;
-  constructor() {
+  constructor() { //public brpk: BarcodePicker
     this.selDate = SimpleCalendarComponent.getToday();
     this.zeropad = SimpleCalendarComponent.zeropad;
     this.today = new Date();
@@ -35,9 +46,11 @@ export class AppComponent {
       primaryForeground: 'black',
       backgroundColor: 'gainsboro',
       previousDateColor: '#989696'//'#74C2D1'
-    }
+    };
     this.highlightToday = true;
+    //this.brpk.pauseScanning(true);
   }
+
   /** Log changes in date */
   dateChanged(date: SimpleDate) {
     console.log(date);
@@ -56,6 +69,7 @@ export class AppComponent {
 
   public showModalDialog(): void {
     this.displayModal = true;
+    //this.brpk.resumeScanning();
   }
 
   public test($event): void {
@@ -66,8 +80,6 @@ export class AppComponent {
 
   public checker(): void {
     this.displayModal = false;
-    console.log(this.dates);
-    console.log(this.dates.length);
   }
 
   formatDate(date): any {
@@ -82,5 +94,18 @@ export class AppComponent {
       day = '0' + day;
     }
     return day + '/' + '/' + month + '/' + date.getFullYear();
+  }
+
+  onScan($event: any): any {
+    console.log($event[0].data);
+    this.val = $event[0].data;
+  }
+
+  onError($event: Error): any {
+    console.log($event);
+  }
+
+  setClear(): void {
+    this.val = '';
   }
 }
---
+++ src/app/calendar/calendar.component.ts
@@ -0,0 +1,190 @@
+import {AfterViewInit, Component, forwardRef, Input, OnInit, ViewChild} from '@angular/core';
+import {DayCalendarComponent, ECalendarMode, IDay, IDayCalendarConfig, IMonth, MonthCalendarComponent} from 'ng2-date-picker';
+import {Moment} from 'moment';
+import * as moment from 'moment';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CalendarComponent),
+      multi: true
+    },
+  ]
+})
+export class CalendarComponent implements OnInit, AfterViewInit, ControlValueAccessor {
+
+  dates: Moment[] = [];
+  config: IDayCalendarConfig = {
+    locale: 'ru',
+    allowMultiSelect: true,
+    monthFormat: 'MMMM, YYYY',
+    yearFormat: 'YYYY',
+    numOfMonthRows: 4,
+    firstDayOfWeek: 'mo'
+  };
+  initCalendar = false;
+  displayedWeekCheckbox: { [key: number]: boolean } = {};
+  currentMonthClicked = false;
+
+  @ViewChild(DayCalendarComponent) calendarComponent: DayCalendarComponent;
+  @ViewChild(MonthCalendarComponent) monthCalendarComponent: MonthCalendarComponent;
+
+  @Input() set minDate(value: string) {
+    this.config = {...this.config, min: value};
+  }
+
+  @Input() set maxDate(value: string) {
+    this.config = {...this.config, max: value};
+  }
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  get weeks() {
+    return this.calendarComponent.weeks;
+  }
+
+  get currentCalendarMode(): ECalendarMode {
+    return this.calendarComponent.currentCalendarMode;
+  }
+
+  get calendarModes() {
+    return this.calendarComponent.CalendarMode;
+  }
+
+  get navLabel(): string {
+    return this.calendarComponent.navLabel;
+  }
+
+  get monthNavLabel(): string {
+    return this.monthCalendarComponent.navLabel;
+  }
+
+  get months() {
+    return this.monthCalendarComponent.yearMonths;
+  }
+
+  get showLeftNav(): boolean {
+    return this.calendarComponent.showLeftNav;
+  }
+
+  get showRightNav(): boolean {
+    return this.calendarComponent.showRightNav;
+  }
+
+  get _currentDateView() {
+    if (this.calendarComponent) {
+      return this.calendarComponent._currentDateView;
+    }
+    return null;
+  }
+
+  get _selected() {
+    if (this.calendarComponent) {
+      return this.calendarComponent._selected;
+    }
+    return null;
+  }
+
+  dayClicked(day: IDay, groupSelect = false) {
+    if (!day.disabled) {
+      this.calendarComponent.dayClicked(day);
+      console.log(day.date.day()+'/'+day.date.month()+'/'+day.date.year());
+    }
+    if (day.prevMonth && !groupSelect) {
+      this.onLeftNavClick();
+    } else if (day.nextMonth && !groupSelect) {
+      this.onRightNavClick();
+    }
+
+  }
+
+  weekClicked(state: boolean, days: IDay[], index: number) {
+    if (days.some(day => !day.disabled)) {
+      this.displayedWeekCheckbox[index] = state;
+      if (state) {
+        days.forEach(day => {
+          if (!day.selected) {
+            this.dayClicked(day, true);
+          }
+        });
+      } else {
+        days.forEach(day => {
+          if (day.selected) {
+            this.dayClicked(day, true);
+          }
+        });
+      }
+    }
+  }
+
+  monthClicked(state: boolean): void {
+    this.currentMonthClicked = state;
+    this.weeks.forEach((week, index) => this.weekClicked(state, week, index));
+  }
+
+  toggleCalendarMode(mode: ECalendarMode): void {
+    this.calendarComponent.toggleCalendarMode(mode);
+  }
+
+  monthSelected(month: IMonth): void {
+    this.calendarComponent.monthSelected(month);
+    this.updateWeekCheckboxes();
+  }
+
+  onLeftNavClick(): void {
+    this.calendarComponent.onLeftNavClick();
+    this.updateWeekCheckboxes();
+  }
+
+  onRightNavClick(): void {
+    this.calendarComponent.onRightNavClick();
+    this.updateWeekCheckboxes();
+  }
+
+  updateWeekCheckboxes(): void {
+    this.currentMonthClicked = false;
+    this.displayedWeekCheckbox = {};
+    this.calendarComponent.weeks.forEach((week, index) => {
+      this.displayedWeekCheckbox[index] = week.some(day => day.selected);
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.initCalendar = true;
+    this.updateWeekCheckboxes();
+  }
+
+  propagateChange = (dates: string[]) => {
+  };
+
+  registerOnChange(fn: any): void {
+    this.propagateChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+  }
+
+  writeValue(dates: string[]): void {
+    if (dates && dates.length) {
+      this.dates = dates.map(date => moment(date));
+    } else {
+      this.dates = [];
+    }
+  }
+
+  modelChange() {
+    this.propagateChange(this.dates.map((date) => date.toISOString()));
+  }
+}
