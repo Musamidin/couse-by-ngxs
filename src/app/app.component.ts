47e39cae71780dca0869717756e5a72573e1d0a0
Musa <m.ahmedov@transcom.gov.kg>
2020-11-12 01:02:45 +0600

    *in new bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import {Component, TemplateRef} from '@angular/core';
-import { SimpleCalendarComponent, HeatMapModel, ColorMapModel } from './simple-calendar/simple-calendar.component';
-import { SimpleDate } from 'src/app/simple-calendar/simple-calendar.component';
-import { Barcode, BarcodePicker, ScanSettings, configure } from 'scandit-sdk-angular';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 @Component({
   selector: 'app-root',
@@ -19,50 +16,13 @@ export class AppComponent {
     this.modalRef = this.modalService.show(template);
   }
 
-  public settings = new ScanSettings({ enabledSymbologies: [
-      Barcode.Symbology.EAN8,
-      Barcode.Symbology.EAN13,
-      Barcode.Symbology.UPCA,
-      Barcode.Symbology.UPCE,
-      Barcode.Symbology.CODE128,
-      Barcode.Symbology.CODE39,
-      Barcode.Symbology.CODE93,
-      Barcode.Symbology.INTERLEAVED_2_OF_5,
-    ] });
-
-  public showModalDialog(): void {
-    this.displayModal = true;
-  }
-
-  public checker(): void {
-    this.displayModal = false;
-  }
-
-  formatDate(date): any {
-    let month = date.getMonth() + 1;
-    let day = date.getDate();
-
-    if (month < 10) {
-      month = '0' + month;
-    }
-
-    if (day < 10) {
-      day = '0' + day;
-    }
-    return day + '/' + '/' + month + '/' + date.getFullYear();
-  }
-
-  onScan($event: any): any {
-    console.log($event[0].data);
-    this.val1 = $event[0].data;
-  }
-
-  onError($event: Error): any {
-    console.log($event);
-  }
-
   setClear(): void {
     this.val1 = '';
     this.val2 = '';
+  }
+
+  done(): void {
+    this.modalRef.hide();
+    console.log('test');
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,30 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {DialogModule} from 'primeng/dialog';
-import {ButtonModule} from 'primeng/button';
-import { SimpleCalendarModule } from './simple-calendar/simple-calendar.module';
 import { CalendarModule } from './calendar/calendar.module';
-import { ScanditSdkModule } from 'scandit-sdk-angular';
 import { ModalModule } from 'ngx-bootstrap/modal';
 
-const licenseKey = 'Adlf1iHJPmS9Kx1zuBPjtp0f/YOcRKyxDWlqipsEn/vaR+JaN24dM+lcbA7DYUD9UHGs91tUpfMFbfoxtXwiejJfxMjVdVmlUlJsneRE23hcVk4lxBf08UUKhV50QwQq9hlLYp532WGsdKWeK/LFtUzo1q6Mbm8Fq+33bA4jR2kiU4MqmiAg+OZvmy7sSyLMOWl64XxERSAG6LtKIg/UxQtgBhRxc+VD4KiylJVThq2KYx+HcjuL0QAp7v5UfqPvyb1UP0zaa+LHyF24JS0U8CzQouXSPpnV0J/42hlrmvNnH4mVIoDws7ZrfDQ//WOAGqly67UGxsYIyQBXBYUY2GFQCc23yMCV9XaQ1cXKzaM1xAdn6o0iaN8qyD7aJvf4WJZk3VGYq47Vh+TYAVIhYCnMZTT1rfzit0miAjb/uXLjepHpI+HXVHY7TvT7bCSBzgd+nG6rv/TuXxHd4x81Kian2KxjxI3x4JY2X2wOFuGXc9ahsvWIBQOz0pkmDFJE8iOJWU2VVi4W5yos4IIsdkYpc6oCPrDI8NmcCNRW+n15tY5Lrb7+OslIbowpUWYYHnkf5Pd1YhNOhOC2U+noIlXLH2dI+PnMNxr+ttLXo3qvvo46uNNZojzYZYcBgrkVUnxJNBxXxiFI0xfKqJljcS8NOpFwNKlF/HvzA+f4tQJAxNw6EmQ3aaSnImna4mJoOtl038Qq5RVfiuogFC3JlFcuaTXRAEk7qN80iHeQPEpIu7IAIK9wqGCi3VHinwdSRdVjqZRZrfwxtZqD7KUB828fd6q0ZJDLda1QnXlQNAbxyw==';
-const engineLocation = 'assets/';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    ScanditSdkModule.forRoot(licenseKey, { engineLocation, preloadEngine: true, preloadBlurryRecognition: true }),
     BrowserAnimationsModule,
     AppRoutingModule,
     CalendarModule,
-    DialogModule,
-    ButtonModule,
-    SimpleCalendarModule,
     FormsModule,
-    ModalModule
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
